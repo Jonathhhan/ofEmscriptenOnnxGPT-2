b32dd052fe3f4a97ee20a4e5ac6193c22a8f1bc5
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2023-06-29 00:13:52 +0200

    Add files via upload

--- bin/data/js/main.js
+++ bin/data/js/main.js
@@ -17,30 +17,30 @@ async function gptLoop(textInput, id) {
 	var textOutput = textInput;
 	var generatedToken = 0;
 	var bigInt64Array = [];
-	var tokens = GPTTokenizer_p50k_edit.encode(textOutput);
+	const tokens = GPTTokenizer_p50k_edit.encode(textOutput);
 	for (let i = 0; i < tokens.length; i++) {
 		bigInt64Array[i] = BigInt(tokens[i]);
 	}
 	while (generatedToken < 256) {
-		await delay(0);
+		await new Promise(resolve => setTimeout(resolve, 0));
 		if (id !== calls) break;
 		generatedToken++;
 		bigInt64Array = bigInt64Array.slice(Math.max(bigInt64Array.length - 50, 0))
 		const tensorA = new ort.Tensor("int64", bigInt64Array, [1, 1, bigInt64Array.length]);
 		const feeds = { input1: tensorA };
 		const results = await onnxSession.run(feeds);
-		var dataC = results["output1"].data;
-		var dataA = dataC.slice(50257 * (bigInt64Array.length - 1), 50257 + 50257 * (bigInt64Array.length - 1));
-		var entries = Object.entries(dataA);
-		var sorted = entries.sort((a, b) => b[1] - a[1]);
-		var newToken = parseInt(sorted[randomWithProbability()][0]);
+		const dataC = results["output1"].data;
+		const dataA = dataC.slice(50257 * (bigInt64Array.length - 1), 50257 + 50257 * (bigInt64Array.length - 1));
+		const entries = Object.entries(dataA);
+		const sorted = entries.sort((a, b) => b[1] - a[1]);
+		const newToken = parseInt(sorted[randomWithProbability()][0]);
 		bigInt64Array.push(BigInt(newToken));
 		var newWord = GPTTokenizer_p50k_edit.decode([newToken]);
 		newWord = newWord.replace(/(\r\n|\n|\r)/gm, " ");
 		if (newWord != "<|endoftext|>") {
 			textOutput = textOutput + newWord;
-			var chars = '].!;?)`';
-			var lastChar = textOutput.charAt(textOutput.length - 1);
+			const chars = '].!;?)`';
+			const lastChar = textOutput.charAt(textOutput.length - 1);
 			if (chars.indexOf(lastChar) == 1 || lastChar == "\"") {
 				Module.onnx("inference", textOutput);
 			}
@@ -52,12 +52,8 @@ async function gptLoop(textInput, id) {
 	}
 }
 
-function delay(time) {
-	return new Promise(resolve => {setTimeout(resolve, time)});
-}
-
 function randomWithProbability() {
-	var notRandomNumbers = [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 3];
-	var idx = Math.floor(Math.random() * notRandomNumbers.length);
+	const notRandomNumbers = [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 3];
+	const idx = Math.floor(Math.random() * notRandomNumbers.length);
 	return notRandomNumbers[idx];
 }
