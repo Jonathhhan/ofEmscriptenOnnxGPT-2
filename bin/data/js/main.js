62d43f4acf342eb8a0fbb65be61ad8bec6b4d782
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2023-06-28 23:17:29 +0200

    Add files via upload

--- bin/data/js/main.js
+++ bin/data/js/main.js
@@ -14,34 +14,37 @@ onnxInference = function(textInput) {
 
 async function gptLoop(textInput, id) {
 	Module.onnx("inference", textInput);
+	var textOutput = textInput;
 	var generatedToken = 0;
+	var bigInt64Array = [];
+	var tokens = GPTTokenizer_p50k_edit.encode(textOutput);
+	for (let i = 0; i < tokens.length; i++) {
+		bigInt64Array[i] = BigInt(tokens[i]);
+	}
 	while (generatedToken < 256) {
 		await delay(0);
 		if (id !== calls) break;
 		generatedToken++;
-		var tokens = GPTTokenizer_p50k_edit.encode(textInput);
-		tokens = tokens.slice(Math.max(tokens.length - 50, 0))
-		var bigInt64Array = [];
-		for (let i = 0; i < tokens.length; i++) {
-			bigInt64Array[i] = BigInt(tokens[i]);
-		}
-		const tensorA = new ort.Tensor("int64", bigInt64Array, [1, 1, tokens.length]);
+		bigInt64Array = bigInt64Array.slice(Math.max(bigInt64Array.length - 50, 0))
+		const tensorA = new ort.Tensor("int64", bigInt64Array, [1, 1, bigInt64Array.length]);
 		const feeds = { input1: tensorA };
 		const results = await onnxSession.run(feeds);
 		var dataC = results["output1"].data;
-		var dataA = dataC.slice(50257 * (tokens.length - 1), 50257 + 50257 * (tokens.length - 1));
+		var dataA = dataC.slice(50257 * (bigInt64Array.length - 1), 50257 + 50257 * (bigInt64Array.length - 1));
 		var entries = Object.entries(dataA);
 		var sorted = entries.sort((a, b) => b[1] - a[1]);
-		var newWord = GPTTokenizer_p50k_edit.decode([parseInt(sorted[randomWithProbability()][0])]);
-		newWord = newWord.replace(/(\r\n|\n|\r)/gm, "");
+		var newToken = parseInt(sorted[randomWithProbability()][0]);
+		bigInt64Array.push(BigInt(newToken));
+		var newWord = GPTTokenizer_p50k_edit.decode([newToken]);
+		newWord = newWord.replace(/(\r\n|\n|\r)/gm, " ");
 		if (newWord != "<|endoftext|>") {
-			textInput = textInput + newWord;
+			textOutput = textOutput + newWord;
 			var chars = '].!;?)`';
-			var lastChar = textInput.charAt(textInput.length - 1);
+			var lastChar = textOutput.charAt(textOutput.length - 1);
 			if (chars.indexOf(lastChar) == 1 || lastChar == "\"") {
-				Module.onnx("inference", textInput);
+				Module.onnx("inference", textOutput);
 			}
-			console.log("Loop number:", calls, "Token number:", generatedToken, "Generated text:", textInput);
+			console.log("Loop number:", calls, "Token number:", generatedToken, "Generated text:", textOutput);
 		} else {
 			console.log("End of text!");
 			break;
